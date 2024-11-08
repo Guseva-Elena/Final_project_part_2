eccb2ff918ee04704016d3c4098e5a2d588e8ed2
Elena Guseva <yagusevaes@gmail.com>
2024-11-08 16:00:12 +0300

    Добавлены картинки

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://dc3187c1-62f9-4500-bc7a-35b0b66075b5.serverhub.praktikum-services.ru" # вставить ссылку с тренажера
+URL_SERVICE = " https://fdd10cf7-f968-4fa3-bf97-6a21b36501d7.serverhub.praktikum-services.ru" # вставить ссылку с тренажера
 # Создание нового заказа
 CREATE_ORDER = "/api/v1/orders"
 GET_TRACK_ORDER = "/api/v1/orders/track?t={track}"
