4900f2d0bdece06886689d556534511f6028df1a
Elena Guseva <yagusevaes@gmail.com>
2024-11-08 15:47:26 +0300

    Добавлены картинки

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://c5369d58-14cb-4cdb-a3b4-face1be1d185.serverhub.praktikum-services.ru" # вставить ссылку с тренажера
+URL_SERVICE = "https://dc3187c1-62f9-4500-bc7a-35b0b66075b5.serverhub.praktikum-services.ru" # вставить ссылку с тренажера
 # Создание нового заказа
 CREATE_ORDER = "/api/v1/orders"
 GET_TRACK_ORDER = "/api/v1/orders/track?t={track}"
--- sendor_stand_request.py
+++ sendor_stand_request.py
@@ -1,5 +1,3 @@
-import json
-
 import configuration
 import data
 import requests
